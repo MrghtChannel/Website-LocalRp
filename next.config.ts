8aa673b056a56efe5da5eac03ff31052071faf7d
MrghtChannel <126418894+MrghtChannel@users.noreply.github.com>
2025-02-26 13:43:43 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,28 +1,25 @@
-const TerserPlugin = require("terser-webpack-plugin");
+import type { NextConfig } from "next";
+import TerserPlugin from "terser-webpack-plugin";
 
-const nextConfig = {
+const nextConfig: NextConfig = {
   webpack: (config, { isServer }) => {
     if (!isServer) {
-      config.optimization = config.optimization || {};
-      config.optimization.minimize = true;
-      config.optimization.minimizer = config.optimization.minimizer || [];
-      config.optimization.minimizer.push(
-        new TerserPlugin({
-          terserOptions: {
-            compress: {
-              drop_console: true,
-              drop_debugger: true,
-              pure_funcs: ["console.log", "console.warn", "console.error"],
+      config.optimization = {
+        ...config.optimization,
+        minimizer: [
+          ...(config.optimization?.minimizer || []),
+          new TerserPlugin({
+            terserOptions: {
+              output: {
+                comments: false,
+              },
             },
-            output: {
-              comments: false,
-            },
-          },
-        })
-      );
+          }),
+        ],
+      };
     }
     return config;
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
